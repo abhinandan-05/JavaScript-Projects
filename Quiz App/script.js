5d9f70363ffb6e05540d118499c4a439e23ae8db
abhinandan-05 <abhicpr05@gmail.com>
2024-06-10 13:16:03 +0900

    Quiz app

--- Quiz App/script.js
+++ Quiz App/script.js
@@ -1,6 +1,66 @@
 const questions = [
   {
-    questions: "Which is largest animal in the world?"
-    an
+    questions: "Which is largest animal in the world?",
+    answers: [
+      {text: "Shark", correct: false},
+      {text: "Blue whale", correct: true},
+      {text: "Elephant", correct: false},
+      {text: "Giraffe", correct: false},
+
+    ]
+  }, 
+  {
+    questions: "Which is smallest continent in the world?",
+    answers: [
+      {text: "Asia", correct: false},
+      {text: "Blue whale", correct: true},
+      {text: "Elephant", correct: false},
+      {text: "Giraffe", correct: false},
+    ]
+  },
+  {
+    questions: "Which is smallest continent in the world?",
+    answers: [
+      {text: "Asia", correct: false},
+      {text: "Blue whale", correct: true},
+      {text: "Elephant", correct: false},
+      {text: "Giraffe", correct: false},
+    ]
+  },
+  {
+    questions: "Which is smallest continent in the world?",
+    answers: [
+      {text: "Asia", correct: false},
+      {text: "Blue whale", correct: true},
+      {text: "Elephant", correct: false},
+      {text: "Giraffe", correct: false},
+    ]
   }
-]
+];
+
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+  currentQuestionIndex = 0;
+  score = 0;
+  nextButton.innerHTML = "Next";
+  showQuestion();
+}
+
+function showQuestion() {
+  let currentQuestionIndex = questions[currentQuestionIndex];
+  let questionNo = currentQuestionIndex +1;
+  questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+  currentQuestion.answers.array.forEach(answer => {
+    const button = document.createElement("button");
+    button.innerHTML = answer.text;
+    button.classList.add('btn');
+    answerButton.appendChild(button);
+  });
+}
